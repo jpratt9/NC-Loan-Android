d686b6618f6edf9a0a9a1abb4edb9dd9c92c851e
Jason Yeomans <jyeo098@gmail.com>
2016-10-29 14:21:24 -0400

    initial commit

---
+++ app/src/main/java/me/floatr/util/PreferenceNames.java
@@ -0,0 +1,15 @@
+package me.floatr.util;
+
+/**
+ * Created by jason on 10/29/16.
+ */
+
+public class PreferenceNames {
+    public static final String MAIN_PREFS_NAME = "basic_pref";
+
+    //USER PREF
+    public static final String PREF_USER_AUTH_TYPE = "user_auth_type";
+    public static final String PREF_USER_DISPLAY_NAME = "user_display_name";
+    public static final String PREF_USER_EXT_URL = "user_ext_url";
+    public static final String PREF_USER_IMAGE_URL = "user_image_url";
+}
