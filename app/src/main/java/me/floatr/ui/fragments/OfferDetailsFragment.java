81212745dd1344034b60acc0eb44c9ac39abf401
pedyluis <pedroiv.pd@gmail.com>
2016-10-30 10:28:15 -0400

    offer detail

--- app/src/main/java/me/floatr/ui/fragments/OfferDetailsFragment.java
+++ app/src/main/java/me/floatr/ui/fragments/OfferDetailsFragment.java
@@ -1,8 +1,47 @@
 package me.floatr.ui.fragments;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * Created by pedrod on 10/30/16.
- */
+import me.floatr.R;
+import me.floatr.ui.activities.MainActivity;
+import me.floatr.util.PreferenceNames;
 
-public class OfferDetailsFragment {
+public class OfferDetailsFragment extends Fragment implements View.OnClickListener {
+
+    private View view;
+    private MainActivity mainActivity;
+    private SharedPreferences mainPref, groupPref;
+    Spinner dropdown;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) getActivity();
+        mainPref = mainActivity.getSharedPreferences(PreferenceNames.MAIN_PREFS_NAME, 0);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.createoffer_fragment,
+                container, false);
+        mainActivity.getSupportActionBar().setTitle("Create Loan Offer");
+
+
+        this.view = view;
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+//        if (v == view.findViewById(R.id.button)) {
+//        }
+    }
 }
