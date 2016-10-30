f3976eced460a9d8337be76073f10695a1a5f85f
Jason Yeomans <jyeo098@gmail.com>
2016-10-30 04:39:30 -0400

    fix loanoffer

--- app/src/main/java/me/floatr/models/LoanOffer.java
+++ app/src/main/java/me/floatr/models/LoanOffer.java
@@ -1,15 +1,16 @@
 
 
-        package me.floatr.models;
-        import com.google.gson.annotations.Expose;
-        import com.google.gson.annotations.SerializedName;
+package me.floatr.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 
 public class LoanOffer {
 
     @SerializedName("loaner")
     @Expose
-    private Loaner loaner;
+    private User loaner;
     @SerializedName("min_offer")
     @Expose
     private Integer minOffer;
@@ -27,108 +28,84 @@ public class LoanOffer {
     private String periodUnit;
 
     /**
-     *
-     * @return
-     * The loaner
+     * @return The loaner
      */
-    public Loaner getLoaner() {
+    public User getLoaner() {
         return loaner;
     }
 
     /**
-     *
-     * @param loaner
-     * The loaner
+     * @param loaner The loaner
      */
-    public void setLoaner(Loaner loaner) {
+    public void setLoaner(User loaner) {
         this.loaner = loaner;
     }
 
     /**
-     *
-     * @return
-     * The minOffer
+     * @return The minOffer
      */
     public Integer getMinOffer() {
         return minOffer;
     }
 
     /**
-     *
-     * @param minOffer
-     * The min_offer
+     * @param minOffer The min_offer
      */
     public void setMinOffer(Integer minOffer) {
         this.minOffer = minOffer;
     }
 
     /**
-     *
-     * @return
-     * The maxOffer
+     * @return The maxOffer
      */
     public Integer getMaxOffer() {
         return maxOffer;
     }
 
     /**
-     *
-     * @param maxOffer
-     * The max_offer
+     * @param maxOffer The max_offer
      */
     public void setMaxOffer(Integer maxOffer) {
         this.maxOffer = maxOffer;
     }
 
     /**
-     *
-     * @return
-     * The interestRate
+     * @return The interestRate
      */
     public Integer getInterestRate() {
         return interestRate;
     }
 
     /**
-     *
-     * @param interestRate
-     * The interest_rate
+     * @param interestRate The interest_rate
      */
     public void setInterestRate(Integer interestRate) {
         this.interestRate = interestRate;
     }
 
     /**
-     *
-     * @return
-     * The period
+     * @return The period
      */
     public Integer getPeriod() {
         return period;
     }
 
     /**
-     *
-     * @param period
-     * The period
+     * @param period The period
      */
     public void setPeriod(Integer period) {
         this.period = period;
     }
 
     /**
-     *
-     * @return
-     * The periodUnit
+     * @return The periodUnit
      */
     public String getPeriodUnit() {
         return periodUnit;
     }
 
     /**
-     *
-     * @param periodUnit
-     * The period_unit
+     * @param periodUnit The period_unit
      */
     public void setPeriodUnit(String periodUnit) {
         this.periodUnit = periodUnit;
